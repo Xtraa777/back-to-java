7cda4c9922c06b9f93f51264a44b04918d55230e
jun <mazino0588@gmail.com>
2024-11-21 18:02:11 +0900

    relocate

---
+++ src/start/cond/ex/EvenOddEx.java
@@ -0,0 +1,11 @@
+package start.cond.ex;
+
+public class EvenOddEx {
+
+    public static void main(String[] args) {
+        int x = 2;
+
+        String result = (x % 2 == 0) ? "even" : "odd";
+        System.out.println(result);
+    }
+}
