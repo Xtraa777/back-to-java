b085f34e27ad23c9ca911ce1212db02e7d480802
jun <mazino0588@gmail.com>
2024-11-22 19:00:49 +0900

    상품 관리 프로그램 만들기

---
+++ src/start/array/ex/ProductAdminEx.java
@@ -0,0 +1,58 @@
+package start.array.ex;
+
+import java.util.Scanner;
+
+public class ProductAdminEx {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maxProduct = 10;
+        String[] productNames = new String[maxProduct];
+        int[] productPrices = new int[maxProduct];
+        int productCount = 0;
+
+        while (true) {
+            System.out.print("1. 상품 등록 | 2. 상품 목록 | 3. 종료\n메뉴를 선택하세요: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    if (productCount < maxProduct) {
+                        System.out.print("상품 이름을 입력하세요: ");
+                        productNames[productCount] = sc.nextLine();
+
+                        System.out.print("상품 가격을 입력하세요: ");
+                        productPrices[productCount] = sc.nextInt();
+
+                        productCount++;
+                        continue;
+                    } else {
+                        System.out.println("더 이상 상품을 등록할 수 없습니다.");
+                        continue;
+                    }
+
+                case 2:
+                    if (productCount == 0) {
+                        System.out.println("등록된 상품이 없습니다.");
+                        continue;
+                    } else {
+                        for (int i = 0; i < productCount; i++) {
+                            System.out.println(productNames[i] + ": " + productPrices[i] + "원");
+                        }
+                        continue;
+                    }
+
+                case 3:
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+
+                default:
+                    System.out.println("잘못된 메뉴를 선택하셨습니다.");
+                    continue;
+            }
+            break;
+        }
+
+    }
+}
