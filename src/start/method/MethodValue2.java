3511a051c1576209086ccdca01a44677423870c5
jun <mazino0588@gmail.com>
2024-11-23 18:59:46 +0900

    method

---
+++ src/start/method/MethodValue2.java
@@ -0,0 +1,17 @@
+package start.method;
+
+public class MethodValue2 {
+
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("1. changeNum 호출 전, number: " + number);
+        changeNum(number);
+        System.out.println("4. changeNum 호출 후, number: " + number);
+    }
+
+    public static void changeNum(int number) {
+        System.out.println("2. changeNum 변경 전, number: " + number);
+        number = number * 2;
+        System.out.println("3. changeNum 변경 후, number: " + number);
+    }
+}
