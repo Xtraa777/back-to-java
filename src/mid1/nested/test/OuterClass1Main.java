3044ba4a9b7c38be58e7c219c87903a45fee1f90
jun <mazino0588@gmail.com>
2024-12-10 18:51:56 +0900

    nestedTest1

---
+++ src/mid1/nested/test/OuterClass1Main.java
@@ -0,0 +1,10 @@
+package mid1.nested.test;
+
+public class OuterClass1Main {
+
+    public static void main(String[] args) {
+        OuterClass1.NestedCalss nested = new OuterClass1.NestedCalss();
+
+        nested.hello();
+    }
+}
