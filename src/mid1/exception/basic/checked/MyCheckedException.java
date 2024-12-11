ff1edbec442e22d3a3e76b72768803f8d6ffdd3b
jun <mazino0588@gmail.com>
2024-12-11 22:09:53 +0900

    exception1

---
+++ src/mid1/exception/basic/checked/MyCheckedException.java
@@ -0,0 +1,7 @@
+package mid1.exception.basic.checked;
+
+public class MyCheckedException extends Exception{
+    public MyCheckedException(String message) {
+        super(message);
+    }
+}
---
+++ src/mid1/exception/basic/unchecked/MyUncheckedException.java
@@ -0,0 +1,8 @@
+package mid1.exception.basic.unchecked;
+
+public class MyUncheckedException extends RuntimeException {
+
+    public MyUncheckedException(String message) {
+        super(message);
+    }
+}
